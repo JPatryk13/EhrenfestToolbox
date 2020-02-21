7fd0c65e07e83d8055f7a1ffdddc53adaf1447f8
Patryk Jesionka [paj24] <paj24@aber.ac.uk>
2020-02-21 07:47:34 +0000

    Updated header

--- toolbox/Wavefunction.m
+++ toolbox/Wavefunction.m
@@ -1,4 +1,6 @@
-% WAVEFUNCTION - 
+% WAVEFUNCTION - generating data required to plot a flower-shaped graph of
+% the wavefunction. It draws the aplitude in the same plane as the circular
+% trajectory of a particle.
 %
 %   obj = Wavefunction(radius, quantumN), constructor,
 %       validates user input, defines base properties of the wavefunction
