bea2ad69fc0cf46084d8ad4ad4143dd870d7de8d
Patryk Jesionka [paj24] <paj24@aber.ac.uk>
2020-02-21 04:32:08 +0000

    Added header

--- toolbox/Spiral.m
+++ toolbox/Spiral.m
@@ -1,13 +1,73 @@
-% 
-%      INSTRUCTION
-% Call in the order:
-% 1. Spiral - contructor
-% 1a. setCentrePoint (optional)
-% 2. setData
-% 3. create
-% 4. plotSpiral
+% SPIRAL - generating data required to plot a spiral-shaped trajectory
+% in two- or three-dimensional space.
 %
+%   obj = Spiral(rMin, rMax, centrePoint, fixedCoordinate, noOfLaps), constructor,
+%       validates user input, generates arrays of coordinates*, based on the
+%       'fixedCoordinate' decides on the orientation of the spiral in 3D 
+%       space and assigns data (with determined prefered size of the plot) 
+%       to the structure.
+%       *Approach similar to the Circle class'; however, radius range and
+%       number of laps is a factor in the coordinates generation.
+%           Input:
+%       'rMin', 'rMax': nonnegative numbers ('rMax' must be greater than
+%       'rMin'), range of radii in between which the spiral is plotted.
+%       'centrePoint': two- or three-dimensional array of numeric values,
+%       dimensionality forces dimensionality of the 'spiral' structure's
+%       arrays and thus whether the plot will be 2D or 3D; it is the entre 
+%       of a spiral.
+%       'fixedCoordinate': must be a character 'x', 'y' or 'z'; the spiral
+%       plotted in 3D though as a plot has two dimensions - the letter here
+%       states the axis in along which the spiral should be flat. It is
+%       related to the limitation of the class.
+%           Output:
+%       'obj': object of the class
 %
+%   spiral = getSpiral(obj), extract generated data
+%           Input:
+%       'obj': object of the class
+%           Output:
+%       'spiral': structure containing 'coordinates' and 'size' arrays.
+%
+%   [a, b] = generateSpiral(obj), private method, performing an actual
+%       generation of the spiral coordinates based on the radii range and 
+%       angle array.
+%           Input:
+%       'obj': object of the class
+%           Output:
+%       [a, b]: a pair of arrays with coordinates
+%
+%   Limitations:
+%       Generated spiral cannot be oriented freely in 3D space. I.e.
+%       the axis going through the centre (perpendicular to the surface the 
+%       spiral is plotted on) must be parallel to one of the axes.
+%
+%   Examples:
+%       Create a spiral in 2D (x-y plane) with radii range from 3 to 5, 
+%       centred at [0, 0] and 1 lap.
+%           spiralHandle = Spiral(3, 5, [0 0], 'z', 1)
+%           spiral = getSpiral(spiralHandle)
+%           plot(spiral.coordinates{1}, spiral.coordinates{2})
+%           axis(spiral.size)
+%       Create a spiral in 3D (fixed x coordinate) with radius ranging from
+%       6.28 to 12.56 centred at [0, 3.14, 3.14] - it is plotted on the y-z 
+%       surface at x = 0 - and 4 laps.
+%           spiralHandles = Spiral(6.28, 12.56, [0 3.14 3.14], 'x', 4)
+%           spiral = getSpiral(spiralHandle)
+%           plot3(spiral.coordinates{1}, spiral.coordinates{2}, 
+%                 spiral.coordinates{3})
+%           axis(spiral.size)
+%
+%   Use:
+%       Such a structure ('spiral') can be fed into standard MATLAB
+%       functions (e.g. plot(), plot3()). However, its purpose is to input
+%       data into the PlotToolbox's PLOT.
+%
+%   See also:
+%       PARABOLOID, SPIRAL, WAVEFUNCTION, PLOT, QUANTUMN,
+%       ENERGYAPPROXIMATION
+%
+%   Patryk Jesionka, 2019
+
 classdef Spiral
     properties
         % rmax (later r), rmin: 'starting' and 'ending' radii of the spiral
@@ -94,8 +154,8 @@ classdef Spiral
             end
         end
         
-        function sprl = getSpiral(obj)
-            sprl = obj.spiral;
+        function spiral = getSpiral(obj)
+            spiral = obj.spiral;
         end
     end
 end
