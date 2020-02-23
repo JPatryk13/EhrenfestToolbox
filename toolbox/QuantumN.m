4384f77cbea0bfd9165fae474f327107d5a00c67
Patryk Jesionka [paj24] <paj24@aber.ac.uk>
2020-02-23 05:58:37 +0000

    Updated comments

--- toolbox/QuantumN.m
+++ toolbox/QuantumN.m
@@ -1,7 +1,7 @@
 classdef QuantumN
     properties
-        hbar = 1.05*10.^(-34); % modified Planck's constant
-        me = 9.1094*10.^(-31); % electron rest mass
+        hbar = 1.05*10.^(-34); % Modified Planck's constant
+        me = 9.1094*10.^(-31); % Electron rest mass
         linearSpeed {mustBePositive} = 1
         radius {mustBePositive} = 1
         initialEnergy
@@ -14,27 +14,43 @@ classdef QuantumN
             obj.linearSpeed = linearSpeed;
             obj.radius = radius;
             
-            obj.initialEnergy = (obj.me*obj.linearSpeed^2)/2; % classical energy
+            % Classical kinetic (initial) energy based on the linear speed
+            % of an electron
+            obj.initialEnergy = (obj.me*obj.linearSpeed^2)/2;
+            % Quantum number based on the classical energy and circular
+            % trajectory radius
             obj.quantumN = (obj.radius/obj.hbar)*sqrt(2*obj.me*obj.initialEnergy);
             
+            % Loop through as long as quantum number is greater or equal 2
             while obj.quantumN >= 2
                 if mod(floor(obj.quantumN), 2) == 0
+                    % Overwrite the quantum number when its floor is an 
+                    % even number
                     obj.quantumN = floor(obj.quantumN);
                 elseif floor(obj.quantumN) >= 2
+                    % Overwrite the quantum number with lower integer when 
+                    % its floor is equal or greater than 2 ??? 
                     obj.quantumN = floor(obj.quantumN) - 1;
                 else
+                    % If neither both, break the loop
                     break
                 end
                 
+                % Add quantum number to the list
                 obj.quantNumbers = [obj.quantNumbers, obj.quantumN];
                 
+                % Overwrite energy with the quantum energy 
+                % subtracted from the initial energy (the value from the  
+                % past iteration or the classical initial energy)
                 obj.initialEnergy = obj.initialEnergy - ((obj.quantumN^2)*(obj.hbar^2))/(2*obj.me*(obj.radius^2));
                 
+                % Based on the energy value, calculate new quantum number
                 obj.quantumN = (obj.radius/obj.hbar)*sqrt(2*obj.me*obj.initialEnergy);
             end
         end
         
         function quantNumbers = getTheList(obj)
+            % Return the list of quantum numbers
             quantNumbers = obj.quantNumbers;
         end
     end
