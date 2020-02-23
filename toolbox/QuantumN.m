a978491431f5c0922f1e372dbf7a460f520bb2cd
Patryk Jesionka [paj24] <paj24@aber.ac.uk>
2020-02-23 06:21:25 +0000

    Added header

--- toolbox/QuantumN.m
+++ toolbox/QuantumN.m
@@ -1,3 +1,47 @@
+% QUANTUMN - based on given linear speed of an electron and radius of its
+% circular path it calcualtes quantum numbers of electron's allowed quantum
+% states.
+%
+%   obj = QuantumN(linearSpeed, radius), constructor, validates input,
+%       calculates initial values of the energy (classical kinetic energy)
+%       and quantum number. It utilises while loop to determine the range
+%       of *allowed quantum numbers.
+%       *Each of quantum numbers must provide a corresponding energy level
+%       so that all the levels fit in the classical kinetic (initial)
+%       energy.
+%           Input:
+%       'linearSpeed': positive number, linear speed of an electron
+%       'radius': positive number, radius of the electron's circular path
+%           Output:
+%       'obj': object of the class
+%
+%   quantNumbers = getTheList(obj), return list of quantum numbers
+%           Input:
+%       'obj': object of the class
+%           Output:
+%       'quantNumbers': list of quantum numbers
+%
+%   Limitations:
+%       N/A
+%
+%   Examples:
+%       Let an electron travel with speed v=15000m/s with circular path of
+%       the radius r=3?m.
+%           quantumN = QuantumN(0.000003, 15000);
+%           list = getTheList(quantumN);
+%       Output: 390    16     6     4     2
+%
+%   Use:
+%       Such a list (each element) can be fed into WAVEFUNCTION object 
+%       to generate allowed wavefunctions of an electron (taking a circular
+%       path in a mangetic field).
+%
+%   See also:
+%       PARABOLOID, SPIRAL, WAVEFUNCTION, PLOT, CIRCLE,
+%       ENERGYAPPROXIMATION
+%
+%   Patryk Jesionka, Maciej Makuch, 2019
+
 classdef QuantumN
     properties
         hbar = 1.05*10.^(-34); % Modified Planck's constant
