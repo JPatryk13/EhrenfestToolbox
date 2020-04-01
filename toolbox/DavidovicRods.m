68d086df3cab287469bb1f0dea991fc49de596dd
Patryk Jesionka [paj24] <paj24@aber.ac.uk>
2020-04-01 23:50:27 +0100

    rodLen() is now fully functional

--- toolbox/DavidovicRods.m
+++ toolbox/DavidovicRods.m
@@ -1,8 +1,20 @@
 % DAVIDOVICRODS - 
 %
-%   obj = Gif, constructor.
+%   obj = DavidovicRods(noOfRods, radius, speed), constructor.
+%           Input:
+%       'noOfRods':
+%       'radius':
+%       'speed':
 %           Output:
 %       'obj': object of the class.
+%
+%   starDisk(obj),
+%           Input:
+%       'obj':
+%
+%   gifBoth(obj),
+%           Input:
+%       'obj':
 %
 %   Limitations:
 %       ...
@@ -25,7 +37,20 @@ classdef DavidovicRods
         noOfRods {mustBeGreaterThan(noOfRods, 2), mustBeInteger} % Number of rods
         speed = [] % Array of linear speeds of the rotating disc
         radius {mustBeGreaterThan(radius, 0)} % Radius of the inner circle
+        
         c = 2.998*10^8; % Speed of light
+        
+        % Functions describing Lorenz factor and beta angle (described in the constructor)
+        gammaFunc = @(v, c) 1./(1-(v.^2./c^2)); % Lorenz factor as a function of speed (v)
+        betaFunc = @(g, n) 360./(2*n*g); % Beta angle as a function of Lorenz factor (g)
+        
+        % Properties described in the constructor
+        gamma
+        beta
+        a
+        
+        % A function described length of a rod; r - radius, b - beta angle
+        rodLenFunc = @(r, b) 2*r*tand(b);
     end
     methods
         function obj = DavidovicRods(noOfRods, radius, speed)
@@ -44,10 +69,15 @@ classdef DavidovicRods
             end
             obj.speed = speed;
             
-            % Circle
-            circleHandle = Circle(radius, [0 0], 'z', 360);
-            circle = getCircle(circleHandle);
+            obj.gamma = obj.gammaFunc(obj.speed, obj.c); % Lorentz factor(s)
+            % Angle(s) between inner (pointing a middle point of a
+            % rod) and outer (pointing ends of a rod) circle radii
+            obj.beta = obj.betaFunc(obj.gamma, obj.noOfRods);
             
+            obj.a = obj.radius./cosd(obj.beta); % Outer circle radius
+        end
+        
+        function starDisk(obj)
             % Obtain size of the layout
             if length(obj.speed) == 1
                 m = 1;
@@ -60,34 +90,31 @@ classdef DavidovicRods
                 n = 2;
             end
             
+            % Length of axes
+            axis = [-2*obj.radius 2*obj.radius -2*obj.radius 2*obj.radius];
+            
             % Create layout
             layout = Plot;
             layout = createLayout(layout, m, n);
             for i = 1:length(obj.speed)
-                layout = defineTile(layout, "v=" + changeNotationType(obj.speed(i), 's') + "m/s, r=" + string(obj.radius) + "m", {'x [m]' 'y [m]'}, circle.size);
+                layout = defineTile(layout, "v=" + changeNotationType(obj.speed(i), 's') + "m/s, r=" + string(obj.radius) + "m", {'x [m]' 'y [m]'}, axis);
             end
             
             for i = 1:length(obj.speed)
-                gamma = 1/(1-(obj.speed(i)^2/obj.c^2)); % Lorentz factor
-                % Angle between inner (pointing a middle point of a
-                % rod) and outer (pointing ends of a rod) circle radii
-                beta = 360/(2*obj.noOfRods*gamma);
-                a = obj.radius/cosd(beta); % Outer circle radius
-
                 % Empty array for storing rod's coordinates
                 rod = {};
                 % Empty array for storing radii pointing the middle of each rod coordinates
                 r = {};
                 
                 % Calculate end-point coordinates for rods and radii
-                for j = 1:noOfRods
+                for j = 1:obj.noOfRods
                     ang = (j-1)*(360/obj.noOfRods);
 
                     % Calculating coordinates of each of the end points of the rod
-                    x1 = a*sind(ang - beta);
-                    y1 = a*cosd(ang - beta);
-                    x2 = a*sind(ang + beta);
-                    y2 = a*cosd(ang + beta);
+                    x1 = obj.a(i)*sind(ang - obj.beta(i));
+                    y1 = obj.a(i)*cosd(ang - obj.beta(i));
+                    x2 = obj.a(i)*sind(ang + obj.beta(i));
+                    y2 = obj.a(i)*cosd(ang + obj.beta(i));
 
                     % Calculating coordinates of middle points of the rod
                     x0 = (x1 + x2)/2;
@@ -99,7 +126,7 @@ classdef DavidovicRods
                 end
                 
                 % Add plots to a tile
-                for j = 1:noOfRods
+                for j = 1:obj.noOfRods
                     layout = addPlot(layout, i, rod(j,:), '-', 'k', 4, "Tangent");
                     layout = addPlot(layout, i, r(j,:), '-.', 'b', 0.5, "Radius");
                 end
@@ -115,6 +142,35 @@ classdef DavidovicRods
             % Add title to the layout
             t = findobj('type', 'tiledlayout');
             title(t, "Davidovic rods");
+        end
+        
+        function rodLength(obj)
+            % Difference between each speed value required to obtain
+            % 360-element array of speed values ranging between 0 and
+            % 0.99c
+            deltaSpeed = obj.c/359;
+            % Domains (range of values) of speed, Lorenz factor and beta
+            % angle - nested function separated for the sake of readability
+            speedDomain = 0:deltaSpeed:obj.c;
+            gammaDomain = obj.gammaFunc(speedDomain, obj.c);
+            betaDomain = obj.betaFunc(gammaDomain, obj.noOfRods);
+            
+            % Rod length changing from maximum down to 0
+            rodLenDomain = obj.rodLenFunc(obj.radius, betaDomain);
+            
+            % 
+            rodLen = obj.rodLenFunc(obj.radius, obj.beta);
+            
+            layout = Plot;
+            layout = createLayout(layout, 1, 1);
+            layout = defineTile(layout, "Rod length vs linear speed", {'Speed [m/s]' 'Rod length [m]'}, [0 0 0 0]);
+            layout = addPlot(layout, 1, {speedDomain, rodLenDomain}, '-', 'none', 'r', 0.5, "Change of length of a rod");
+            layout = addPlot(layout, 1, {obj.speed, rodLen}, 'none', 'd', 'k', 0.1, "Rod length for an arbitrary" + newline + "chosen speed values");
+            drawLayout(layout);
+        end
+        
+        function gifBoth(obj)
+            % Code to be finished
         end
     end
 end
