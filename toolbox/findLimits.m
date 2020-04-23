a481f19c19f3cfc30bb61f57be968118bb6e0c0d
Patryk Jesionka [paj24] <paj24@aber.ac.uk>
2020-04-23 22:23:19 +0100

    Initial commit

---
+++ toolbox/findLimits.m
@@ -0,0 +1,56 @@
+% FINDLIMITS - finding maximum and minimum values in a given array.
+%
+%   lim = findLimits(coordinate)
+%   lim = findLimits(coordinate, displacement)
+%
+%           Input parameters' names:
+%       'coordinate':       (required), array with numeric values.
+%       'displacement':     0 (default), shift that should be applied to
+%                           the 'coordinate' array.
+%           Output:
+%       'lim':              array with limits, [min max].
+%
+%   Limitations:
+%       Does not support multiple array-displacement pair input.
+%
+%   Examples:
+%       A = [-1 0 1 2 3 4 5 6];
+%       dA = -2;
+%       findLimits(A, dA)
+%
+%   Use:
+%       Extracting min-max limits pair from a numeric array.
+%
+%   See also:
+%       PARABOLOID, SPIRAL, CIRCLE, WAVEFUNCTION, QUANTUMN,
+%       ENERGYAPPROXIMATION, WAVE, PLOT, GIF, DAVIDOVICRODS,
+%       CHANGENOTATIONTYPE
+%
+%   Patryk Jesionka, 2020
+
+function lim = findLimits(coordinate, varargin)
+    % Define default values
+    defaultDisplacement = 0;
+            
+    % Validation functions
+    validCoordinate = @(x) all(isnumeric(x)) && all(isfinite(x)) && all(isreal(x));
+    validDisplacement = @(x) isnumeric(x) && isfinite(x) && isreal(x) && isscalar(x);
+            
+    % Input parser
+    p = inputParser;
+    p.CaseSensitive = true;
+            
+    % Adding arguments
+    addRequired(p, 'coordinate', validCoordinate);
+    addOptional(p, 'displacement', defaultDisplacement, validDisplacement);
+            
+    parse(p, coordinate, varargin{:});
+            
+    % Extract variables from the parser
+    a = p.Results.coordinate;
+    da = p.Results.displacement;
+    
+    % Finding minimum and maximum values in the array; double min/max
+    % function to get rid of the multiplicity of limits.
+    lim = [min(min(a)) + da, max(max(a)) + da];
+end
