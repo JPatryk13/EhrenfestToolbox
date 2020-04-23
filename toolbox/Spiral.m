72043ad242edef612aa779db390f96a231d961f6
Patryk Jesionka [paj24] <paj24@aber.ac.uk>
2020-04-23 16:01:44 +0100

    Added input parser

--- toolbox/Spiral.m
+++ toolbox/Spiral.m
@@ -1,37 +1,47 @@
 % SPIRAL - generating data required to plot a spiral-shaped trajectory
 % in two- or three-dimensional space.
 %
-%   obj = Spiral(rMin, rMax, centrePoint, fixedCoordinate, noOfLaps, q), 
+%   obj = Spiral(rMin, rMax)
+%   obj = Spiral(rMin, rMax, Name, Value) 
 %       constructor, validates user input, generates arrays of 
 %       coordinates*, based on the 'fixedCoordinate' decides on the 
 %       orientation of the spiral in 3D space and assigns data (with 
 %       determined preferred size of the plot) to the structure.
 %       *Approach similar to the Circle class'; however, radius range and
 %       number of laps is a factor in the coordinates generation.
-%           Input:
-%       'rMin', 'rMax': nonnegative numbers ('rMax' must be greater than
-%       'rMin'), range of radii in between which the spiral is plotted.
-%       'centrePoint': two- or three-dimensional array of numeric values,
-%       dimensionality forces dimensionality of the 'spiral' structure's
-%       arrays and thus whether the plot will be 2D or 3D; it is the entre 
-%       of a spiral.
-%       'fixedCoordinate': must be a character 'x', 'y' or 'z'; the spiral
-%       plotted in 3D though as a plot has two dimensions - the letter here
-%       states the axis in along which the spiral should be flat. It is
-%       related to the limitation of the class.
-%       'q': must be positive integer, quality factor (see Updates).
-%           Output:
-%       'obj': object of the class
 %
-%   spiral = getSpiral(obj), extract generated data
-%           Input:
-%       'obj': object of the class
+%           Input parameter names:
+%       'rMin', 'rMax': (required), nonnegative numbers ('rMax' must be
+%                       greater than 'rMin'), range of radii in between
+%                       which the spiral is plotted.
+%       'centrePoint':  [0 0](default), two- or three-dimensional numeric
+%                       row array, number of elements determines whether
+%                       the plot will be 2D or 3D; it is the centre of a
+%                       spiral.
+%       'fixedCoordinate': 'z' (default), 'x', 'y' or 'z'; the spiral
+%                       plotted in 3D though as a plot has two dimensions -
+%                       the letter here states the axis in along which the
+%                       circle should be flat. It is related to the
+%                       limitation of the class.
+%       'noOfLaps':     1 (default), number of laps.
+%       'q':            360 (default), must be positive integer, quality 
+%                       factor (see Updates).
 %           Output:
-%       'spiral': structure containing 'coordinates' and 'size' arrays.
+%       'obj':          object of the class
 %
-%   [a, b] = generateSpiral(obj), private method, performing an actual
-%       generation of the spiral coordinates based on the radii range and 
-%       angle array.
+%   spiral = getSpiral(obj)
+%       extract generated data
+%
+%           Input:
+%       'obj':          object of the class
+%           Output:
+%       'spiral':       structure containing 'coordinates' and 'size'
+%                       arrays.
+%
+%   [a, b] = generateSpiral(obj)
+%       private method, performing an actual generation of the spiral
+%       coordinates based on the radii range and angle array.
+%       
 %           Input:
 %       'obj': object of the class
 %           Output:
@@ -43,25 +53,13 @@
 %       spiral is plotted on) must be parallel to one of the axes.
 %
 %   Examples:
-%       Create a spiral in 2D (x-y plane) with radii range from 3 to 5, 
-%       centred at [0, 0] and 1 lap.
-%           spiralHandle = Spiral(3, 5, [0 0], 'z', 1);
-%           spiral = getSpiral(spiralHandle);
-%           plot(spiral.coordinates{1}, spiral.coordinates{2});
-%           axis(spiral.size)
-%       Create a spiral in 3D (fixed x coordinate) with radius ranging from
-%       6.28 to 12.56 centred at [0, 3.14, 3.14] - it is plotted on the y-z 
-%       surface at x = 0 - and 4 laps.
-%           spiralHandles = Spiral(6.28, 12.56, [0 3.14 3.14], 'x', 4);
-%           spiral = getSpiral(spiralHandle);
-%           plot3(spiral.coordinates{1}, spiral.coordinates{2}, 
-%                 spiral.coordinates{3});
-%           axis(spiral.size)
+%       functions spiralEx1, spiralEx2 in Examples.m
 %
 %   Updates:
 %       01/03/2020: Added quality factor (constructor). Need for unifying
 %           CIRCLE, WAVEFUNCTION and  SPIRAL classes' output number of
 %           steps replaced with freedom of choice.
+%       23/04/2020: Added input parser.
 %
 %   Use:
 %       Such a structure ('spiral') can be fed into standard MATLAB
@@ -76,25 +74,15 @@
 
 classdef Spiral
     properties
+        % Variables required to appear in the private function. Defined as
+        % class' properties for the sake of convenience.
         % rMax (later r), rMin: 'starting' and 'ending' radii of the spiral
-        rMin {mustBePositive}
-        rMax {mustBePositive}
-        
-        % Constant coordinate - determines orientation of the spiral
-        fixedCoordinate {mustBeMember(fixedCoordinate, {'x', 'y', 'z'})} = 'z'
-        % Laps: number of full rotations
-        noOfLaps {mustBeNonnegative}
-        
-        % x, y, z: coordinates of the spiral's centre
-        x {mustBeNumeric} = 0
-        y {mustBeNumeric} = 0
-        z {mustBeNumeric} = 0
-       
-        q {mustBePositive, mustBeInteger} = 360 % Quality factor
+        rMin
+        rMax
         ang % Angle range of a spiral with defined step
         
-        % Structure to be returned with coordinates and dimensions of the
-        % plot
+       % Structure to be returned with coordinates and default dimensions
+       % of the plot
         spiral = struct('coordinates', [], 'size', [])
     end
     methods (Access = private)
@@ -113,27 +101,55 @@ classdef Spiral
         end
     end
     methods (Access = public)
-        function obj = Spiral(rMin, rMax, centrePoint, fixedCoordinate, noOfLaps, q)
-            % Input validation
-            obj.rMin = rMin;
-            obj.rMax = rMax;
-            if ~(obj.rMin < obj.rMax)
-                error("rMin must be smaller than rMax!");
+        function obj = Spiral(rMin, rMax, varargin)
+            % Define default values
+            defaultCentrePoint = [0 0];
+            defaultFixedCoordinate = 'z';
+            defaultNoOfLaps = 1;
+            defaultQ = 360;
+            
+            % Validation functions
+            validRadius = @(x) gt(x, 0) && isreal(x) && isnumeric(x) && isfinite(x) && isscalar(x);
+            validCentrePoint = @(x) isreal(x) && isnumeric(x) && all(isfinite(x)) && isrow(x) && (eq(length(x), 2) || eq(length(x), 3));
+            validFixedCoordinate = @(x) ischar(x) && ismember(x, {'x', 'y', 'z'});
+            validNoOfLaps = @(x) ge(x, 0) && isreal(x) && isnumeric(x) && isfinite(x) && isscalar(x) && eq(x, floor(x));
+            validQ = @(x) validRadius(x) && eq(x, floor(x));
+            
+            % Input parser
+            p = inputParser;
+            p.CaseSensitive = true;
+            
+            % Adding arguments
+            addRequired(p, 'rMin', validRadius);
+            addRequired(p, 'rMax', validRadius);
+            addParameter(p, 'centrePoint', defaultCentrePoint, validCentrePoint);
+            addParameter(p, 'fixedCoordinate', defaultFixedCoordinate, validFixedCoordinate);
+            addParameter(p, 'noOfLaps', defaultNoOfLaps, validNoOfLaps);
+            addParameter(p, 'q', defaultQ, validQ);
+            
+            parse(p, rMin, rMax, varargin{:});
+            
+            % Extract variables from the parser
+            obj.rMin = p.Results.rMin;
+            if p.Results.rMax > obj.rMin % Check if rMax is greater than rMin
+                obj.rMax = p.Results.rMax;
+            else
+                error("rMax must be greater than rMin.");
             end
-            obj.fixedCoordinate = fixedCoordinate;
-            obj.noOfLaps = noOfLaps;
-            if ~(length(centrePoint) == 2 || length(centrePoint) == 3)
-                error("centrePoint array must have 2 or 3 entries!");
-            end
-            obj.x = centrePoint(1);
-            obj.y = centrePoint(2);
+            centrePoint = p.Results.centrePoint;
+            fixedCoordinate = p.Results.fixedCoordinate;
+            noOfLaps = p.Results.noOfLaps;
+            q = p.Results.q;
+            
+            % Separate coordinates of circle's centre
+            x = centrePoint(1);
+            y = centrePoint(2);
             if length(centrePoint) == 3
-                obj.z = centrePoint(3);
+                z = centrePoint(3);
             end
-            obj.q = q;
             
             % Generating an angle range with step 2*pi/90
-            obj.ang = 0:(obj.noOfLaps*2*pi/obj.q):obj.noOfLaps*2*pi;
+            obj.ang = 0:(noOfLaps*2*pi/q):noOfLaps*2*pi;
             
             % Generating coordinate arrays
             [a, b] = obj.generateSpiral;
@@ -141,26 +157,28 @@ classdef Spiral
             
             % Decides whether to return 3d or 2d vector to the structure
             if length(centrePoint) == 2
-                obj.spiral.coordinates = {obj.x+a obj.y+b};
+                obj.spiral.coordinates = {x+a y+b};
+                
                 % Sets optimal size of the plot
-                xlim = [obj.x-2*obj.rMax obj.x+2*obj.rMax];
-                ylim = [obj.y-2*obj.rMax obj.y+2*obj.rMax];
+                xlim = [x-2*obj.rMax x+2*obj.rMax];
+                ylim = [y-2*obj.rMax y+2*obj.rMax];
                 obj.spiral.size = [xlim ylim];
             else
                 % Based on fixedCoordinate sets orientation of the spiral
                 % by assigning a, b and c (c is 'fixed') to appropriate
                 % coordinates x, y and z.
-                if obj.fixedCoordinate == 'x'
-                    obj.spiral.coordinates = {obj.x+c obj.y+a obj.z+b};
-                elseif obj.fixedCoordinate == 'y'
-                    obj.spiral.coordinates = {obj.x+a obj.y+c obj.z+b};
+                if fixedCoordinate == 'x'
+                    obj.spiral.coordinates = {x+c y+a z+b};
+                elseif fixedCoordinate == 'y'
+                    obj.spiral.coordinates = {x+a y+c z+b};
                 else
-                    obj.spiral.coordinates = {obj.x+a obj.y+b obj.z+c};
+                    obj.spiral.coordinates = {x+a y+b z+c};
                 end
+                
                 % Sets optimal size of the plot
-                xlim = [obj.x-2*obj.rMax obj.x+2*obj.rMax];
-                ylim = [obj.y-2*obj.rMax obj.y+2*obj.rMax];
-                zlim = [obj.z-2*obj.rMax obj.z+2*obj.rMax];
+                xlim = [x-2*obj.rMax x+2*obj.rMax];
+                ylim = [y-2*obj.rMax y+2*obj.rMax];
+                zlim = [z-2*obj.rMax z+2*obj.rMax];
                 obj.spiral.size = [xlim ylim zlim];
             end
         end
