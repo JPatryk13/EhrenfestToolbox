09867b44ac766aad98442ab6ed7b45e606e27562
Patryk Jesionka [paj24] <paj24@aber.ac.uk>
2020-04-29 15:34:11 +0100

    added input parser

--- toolbox/Wave.m
+++ toolbox/Wave.m
@@ -1,42 +1,49 @@
 % WAVE - generating data required to plot a graph of the wavefunction with
 % amplitude in z direction.
 %
-%   obj = Wave(radius, quantumN, q), constructor, validates input using CIRCLE
-%       and WAVEFUCNTION classes, generates circle coordinates (x and y of
-%       use) and initialises wavefunction object.
-%           Input:
-%       'radius': nonnegative number, radius of the circle the wavefunction
-%       will be plotted on.
-%       'quantumN': nonnegative, even integer value, quantum number for
-%       frequency, period and wavedomain of the function.
-%       'q': must be positive integer, quality factor (see Updates).
-%           Output:
-%       'obj': object of the class.
+%   obj = Wave(radius, quantumN)
+%   obj = Wave(radius, quantumN, q)
+%       constructor, validates input using CIRCLE and WAVEFUCNTION classes,
+%       generates circle coordinates (x and y of use) and initialises
+%       wavefunction object.
 %
-%   wave = getWave(obj, time, arithmeticType), validates input using
-%       WAVEFUNCTION class, assigns data to the structure.
-%           Input:
-%       'obj': object of the class.
-%       'time': nonnegative value, wavefunction is time dependent therefore
-%       the output coordinates vary with time parameter.
-%       'arithmeticType': 'sin' or 'cos' string, specifies which of the
-%       component of the wavefunction must be returned with the structure.
+%           Input parameters' names:
+%       'radius':       (required), nonnegative number, radius of the
+%                       circle the wavefunction will be plotted on.
+%       'quantumN':     (required), nonnegative, even integer value,
+%                       quantum number for frequency, period and wavedomain
+%                       of the function.
+%       'q':            360 (default), must be positive integer, quality
+%                       factor (see Updates).
 %           Output:
-%       'wave': structure containing 'coordinates' and 'size' arrays.
+%       'obj':          object of the class.
+%
+%   wave = getWave(obj, time)
+%   wave = getWave(obj, time, arithmeticType)
+%       validates input using WAVEFUNCTION class, assigns data to the
+%       structure.
+%
+%           Input parameters' names:
+%       'obj':          object of the class.
+%       'time':         (required), nonnegative value, wavefunction is time
+%                       dependent therefore the output coordinates vary
+%                       with time parameter.
+%       'arithmeticType': 'sin' (default), 'sin' or 'cos' string, specifies
+%                       which of the component of the wavefunction must be
+%                       returned with the structure.
+%           Output:
+%       'wave':         structure containing 'coordinates' and 'size'
+%                       arrays.
 %
 %   Limitations:
 %       Such a wavefunction has it's centre point at [0 0 0] and currently
-%       in cannot be modified.
+%       it cannot be modified.
 %
 %   Examples:
-%       Plot a wavefunction of an electron which energy is described by
-%       n=8' It is travelling around the circular path of radius, r=0.1m.
-%       Desired type of the wavefunction is cosine at time, t=0.
-%           waveHandle = Wave(0.1, 8);
-%           wave = getWave(waveHandle, 0, 'cos');
-%           plot3(wave.coordinates{1}, wave.coordinates{2},
-%                 wave.coordinates{3});
-%           axis(wave.size)
+%       Function waveEx from Examples.m
+%
+%   Updates:
+%       29/04/2020: Added input parser.
 %
 %   Use:
 %       Such a structure ('wave') can be fed into standard MATLAB functions
@@ -45,13 +52,14 @@
 %
 %   See also:
 %       PARABOLOID, SPIRAL, CIRCLE, PLOT, QUANTUMN, ENERGYAPPROXIMATION,
-%       WAVEFUNCTION, GIF, DAVIDOVICRODS, CHANGENOTATIONTYPE
+%       WAVEFUNCTION, GIF, DAVIDOVICRODS, CHANGENOTATIONTYPE, FINDLIMITS
 %
 %   Patryk Jesionka, 2020
 
 classdef Wave
     properties
         wavefunctionHandle % Handle for the Wavefunction object
+        
         % Structure containing coordinates and dimensions of the circle the
         % wavefunction should be plotted on
         circle = struct('coordinates', [], 'size', [])
@@ -59,21 +67,60 @@ classdef Wave
         wave = struct('coordinates', [], 'size', [])
     end
     methods
-        function obj = Wave(radius, quantumN, q)
-            % Usse CIRCLE and WAFUNCTION class objects to verify input and
+        function obj = Wave(radius, quantumN, varargin)
+            % Define default values
+            defaultQ = 360;
+            
+            % Input parser
+            p = inputParser;
+            p.CaseSensitive = true;
+            
+            % Adding arguments
+            addRequired(p, 'radius');
+            addRequired(p, 'quantumN');
+            addOptional(p, 'q', defaultQ);
+            
+            parse(p, radius, quantumN, varargin{:});
+            
+            % Extract variables from the parser
+            radius = p.Results.radius;
+            quantumN = p.Results.quantumN;
+            q = p.Results.q;
+            
+            % Use CIRCLE and WAFUNCTION class objects to verify input and
             % create circle as a base for the wavefunction and to
             % initialise wavefunction object
-            circleHandle = Circle(radius, [0 0 0], 'z', q);
+            circleHandle = Circle(radius, 'q', q);
             obj.circle = getCircle(circleHandle);
-            obj.wavefunctionHandle = Wavefunction(radius, quantumN, q);
+            obj.wavefunctionHandle = Wavefunction(radius, quantumN, 'q', q);
             
             % Set x and y axis limits of the plot
-            obj.wave.size(1:4) = [-radius radius -radius radius].*1.5;
+            xlim = findLimits(obj.circle.coordinates{1});
+            ylim = findLimits(obj.circle.coordinates{2});
+            obj.wave.size(1:4) = [xlim ylim];
         end
         
-        function wave = getWave(obj, time, arithmeticType)
+        function wave = getWave(obj, time, varargin)
+            % Define default values
+            defaultArithmeticType = 'sin';
+            
+            % Input parser
+            p = inputParser;
+            p.CaseSensitive = true;
+            
+            % Adding arguments
+            addRequired(p, 'time');
+            addOptional(p, 'arithmeticType', defaultArithmeticType);
+            
+            parse(p, time, varargin{:});
+            
+            % Extract variables from the parser
+            time = p.Results.time;
+            arithmeticType = p.Results.arithmeticType;
+            
             % Return coordinates and the size from the wavefunction object
-            wavefunction = getWavefunc(obj.wavefunctionHandle, time, arithmeticType, 'z');
+            wavefunction = getWavefunc(obj.wavefunctionHandle, time, 'arithmeticType', arithmeticType,...
+                                                                     'amplitudeAxes', 'z');
             
             % Put data in the structure
             obj.wave.coordinates = {obj.circle.coordinates{1} obj.circle.coordinates{2} wavefunction.coordinates{3}};
